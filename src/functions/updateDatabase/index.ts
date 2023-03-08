e4c11aa6ca48109a22a6f564078bc783b4f88469
Gustavo Ceccon <gustavofceccon@gmail.com>
2023-03-08 20:56:02 -0300

    Update index.ts

--- src/functions/updateDatabase/index.ts
+++ src/functions/updateDatabase/index.ts
@@ -21,31 +21,30 @@ exports.handler = async function (event: any, context: any) {
   const cards: Card[] = [];
   const batchRequests: WriteRequest[] = [];
 
-  const tableName: string =
-    process.env.TABLE_NAME != undefined
-      ? (process.env.TABLE_NAME as string)
-      : "ygoTcg";
-  event.Records.forEach((record: { body: CardInput }) => {
-    const card = new Card(record.body);
+  const tableName: string = process.env.TABLE_NAME || "ygoTcg";
+  console.log(tableName);
+  event.Records.forEach((record: { body: string }) => {
+    const cardInput = JSON.parse(record.body) as CardInput;
+    const card = new Card(cardInput);
     cards.push(card);
+
     batchRequests.push({
       PutRequest: {
         Item: {
-          id: { S: card.id },
+          id: { S: card.id.toString() },
           name: { S: card.name },
           type: { S: card.type },
-          frameType: { S: card.frameType },
           desc: { S: card.desc },
           race: { S: card.race },
-          atk: { N: card.atk + "" },
-          def: { N: card.def + "" },
-          level: { N: card.level + "" },
-          attribute: { S: card.attribute + "" },
-          archetype: { S: card.archetype + "" },
-          scale: { S: card.scale + "" },
-          link: { S: card.link + "" },
+          atk: { N: card.atk?.toString() || "0" },
+          def: { N: card.def?.toString() || "0" },
+          level: { N: card.level?.toString() || "0" },
+          attribute: { S: card.attribute || "" },
+          archetype: { S: card.archetype || "" },
+          scale: { S: card.scale || "" },
+          link: { S: card.link || "" },
           linkMarkers: {
-            SS: card.linkMarkers != null ? card.linkMarkers : [],
+            SS: card.linkMarkers || [""],
           },
           cardSets: { SS: card.cardSets.map((set) => set.setName) },
           cardImages: { SS: card.cardImages.map((img) => img.imageUrl) },
@@ -55,33 +54,14 @@ exports.handler = async function (event: any, context: any) {
   });
 
   const db = new DynamoDBClient({ region: "us-east-2" });
-  const results = db.send(
+  const results = await db.send(
     new BatchWriteItemCommand({
       RequestItems: {
-        tableName: batchRequests,
+        "ygoTcg": batchRequests
       },
     })
   );
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(cards.map((card) => {
-      id: card.id
-      name: card.name
-      type: card.type
-      frameType: card.frameType
-      desc: card.desc
-      race: card.race
-      atk: card.atk
-      def: card.def
-      level: card.level
-      attribute: card.attribute
-      archetype: card.archetype
-      scale: card.scale
-      link: card.link
-      linkMarkers: card.linkMarkers
-      cardSets: card.cardSets
-      cardImages: card.cardImages
-    })),
-  };
+  if(results)
+    console.log("RESULTS", JSON.stringify(results));
 };
